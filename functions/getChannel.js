edf61e30846ae356c5ceb7222c0f489a50308d3f
SoulYT1534 <thica.doi@gmail.com>
2023-08-03 20:53:52 +0700

    testing

--- functions/getChannel.js
+++ functions/getChannel.js
@@ -1,6 +1,6 @@
 const client = require("../index")
 module.exports = function getChannel(nameOrID) {
-    const channel = client.channels.cache ? client.channels.cache.find(ch => ch.name === nameOrID) || client.channels.cache.get(nameOrID) : client.guilds.cache.find(guild => guild.name === "CLIMAX").channels.cache.find(ch => ch.name === nameOrID) || client.guilds.cache.find(guild => guild.name === "CLIMAX").channels.cache.get(nameOrID)
+    const channel = client.channels.cache.find(ch => ch.name === nameOrID) || client.channels.cache.get(nameOrID)
     if(!channel) {
       console.error("ERROR: [getChannel.js]\nFailed to get channel.\nReason: [Invalid name.]")
       process.exit()
--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.slashCommands = new Collection();
 const prefix = client.config.bot.prefix;
 
 const db = new QuickDB(); // Creates a json.sqlite file in the root folder
-
+console.log(client.channels.cache)
 client.getFunctions = function () {
   const functions = fs.readdirSync('./functions').filter(file => file.endsWith('.js')).map(file => {
     const functionName = file.slice(0, -3); // Extract the function name from the file name (excluding the ".js" extension)

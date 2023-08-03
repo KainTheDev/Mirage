6dcb1d88421c71fdd0819d993bdb17667bf0d507
SoulYT1534 <thica.doi@gmail.com>
2023-08-03 21:34:21 +0700

    instead of automatically sending the message, i will use a type of command

--- events/createSpamCmdChannel.js
+++ events/createSpamCmdChannel.js
@@ -1,5 +1,6 @@
 module.exports = ({client}) => {
-    client.on("ready", () => {
+    client.on("messageCreate", (message) => {
+    if(message.guild.name !== "CLIMAX") return;
     const {getChannel} = client.getFunctions()
     const { ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
     const channel = getChannel("create-channel")
@@ -8,10 +9,30 @@ module.exports = ({client}) => {
         new ButtonBuilder()
         .setEmoji("➕")
         .setLabel("Create")
-        .setCustomId("CREATE")
+        .setCustomId("create_channel")
         .setStyle(ButtonStyle.Success)
         )
     // Send a message using the webhook
-    if([...channel.messages.cache].length === 0) return channel.send({content: "Click to the button to create a new channel! (customizable)", components: [row]});
+    if(message.content.includes("#SEND_WEBHOOK") && message.author.id === message.guild.ownerId) return channel.send({content: "Click to the button to create a new channel! (customizable)", components: [row]});
 })
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isButton()) return;
+  
+    if (interaction.customId === 'create_channel') {
+      const guild = client.guilds.cache.find(guild => guild.name === "CLIMAX");
+      const channelName = interaction.user.username+' - Channel'; // Change this to the desired channel name
+  
+      try {
+        const createdChannel = await guild.channels.create(channelName, {
+          type: 'GUILD_TEXT', // Change this to the desired channel type
+        });
+  
+        await interaction.reply({ content: `Channel ${createdChannel} has been created!`, ephemeral: true });
+      } catch (error) {
+        console.error('Error creating channel:', error);
+        await interaction.reply({ content: 'An error occurred while creating the channel.', ephemeral: true });
+      }
+    }
+  });
 }

939595dd25fc9a184820a8d63530f31455e26028
SoulYT1534 <thica.doi@gmail.com>
2023-08-03 19:56:00 +0700

    its a big lag so i fixed that

--- events/loadCommands.js
+++ events/loadCommands.js
@@ -2,7 +2,6 @@ module.exports = async ({client}) => {
 client.on('ready', async () => {
   let slashCommands = client.slashCommands.map(slashCommand => slashCommand)
   if(slashCommands[0]) {
-  await client.application.commands.set([])
   await client.guilds.cache
     .get(client.guilds.cache.find(guild => guild.name === "CLIMAX").id)
     .commands.set(slashCommands);

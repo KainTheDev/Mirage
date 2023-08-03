39db38fbd9cf18f4dd0085b74eac025a0e0af917
SoulYT1534 <thica.doi@gmail.com>
2023-08-03 20:43:13 +0700

    testing

--- functions/getChannel.js
+++ functions/getChannel.js
@@ -1,7 +1,7 @@
 const client = require("../index")
 module.exports = function getChannel(nameOrID) {
+  console.log([...client.channels.cache])
     const channel = client.channels.cache.find(ch => ch.name === nameOrID) || client.channels.cache.get(nameOrID)
-    console.log(nameOrID)
     if(!channel) {
       console.error("ERROR: [getChannel.js]\nFailed to get channel.\nReason: [Invalid name.]")
       process.exit()

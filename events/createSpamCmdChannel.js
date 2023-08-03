b5b9a969e25e6e9b6ae233600a670b45751f5051
SoulYT1534 <thica.doi@gmail.com>
2023-08-03 20:25:10 +0700

    created new file

---
+++ events/createSpamCmdChannel.js
@@ -0,0 +1,16 @@
+module.exports = ({client}) => {
+    const {getChannel} = client.getFunctions()
+    const { ButtonBuilder, ButtonStyle, ActionRowBuilder, WebhookClient } = require('discord.js');
+    const channel = getChannel("create-channel")
+    const webhookClient = new WebhookClient({ url: "https://discord.com/api/webhooks/1136649645557305384/_l_EVWPMdRD4xlIwHkjPVvCjSzIk-hEDnVZieI7zyF-TGmyIFn1JiH8Qt1_4ZMxuX6nm" });
+    const row = new ActionRowBuilder()
+    .setComponents(
+        new ButtonBuilder()
+        .setEmoji("➕")
+        .setLabel("Create")
+        .setCustomId("CREATE")
+        .setStyle(ButtonStyle.Success)
+        )
+    // Send a message using the webhook
+    if(channel.messages.cache.length === 0) return webhookClient.send({components: [row]});
+}

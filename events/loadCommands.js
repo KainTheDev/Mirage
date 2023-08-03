54d6a2aa8c564364fca40ab4ce6fd51b3b716186
KainTheDev <thica.doi@gmail.com>
2023-08-03 11:17:08 +0700

    Only load slash commands to CLIMAX server

--- events/loadCommands.js
+++ events/loadCommands.js
@@ -2,7 +2,10 @@ module.exports = async ({client}) => {
 client.on('ready', async () => {
   let slashCommands = client.slashCommands.map(slashCommand => slashCommand)
   if(slashCommands[0]) {
-  await client.application.commands.set(slashCommands)
+  await client.application.commands.set([])
+  await client.guilds.cache
+    .get(client.guilds.cache.find(guild => guild.name === "CLIMAX"))
+    .commands.set(slashCommands);
   console.log("Loaded slash commands.")
   }else{
     slashCommands = []

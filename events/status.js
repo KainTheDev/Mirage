f5cf0fad628530f3aff21f795212b9711de90077
KainTheDev <89621266+KainTheDev@users.noreply.github.com>
2023-08-25 13:11:38 +0000

    changed status file

--- events/status.js
+++ events/status.js
@@ -1,7 +1,7 @@
 module.exports = ({client}) => {
   const {ActivityType} = require("discord.js")
   client.once('ready', () => {
-    console.log('Bot is ready!');
+    console.log('Bot is ready! ' + client.user.tag);
     client.user.setPresence({
   activities: [{ name: `you and others`, type: ActivityType.Watching }],
   status: 'online',
